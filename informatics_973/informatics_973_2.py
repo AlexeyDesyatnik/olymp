f3e4fdfc996e5573542d39145977e4573a134a1b
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-09-23 21:04:02 +0300

    973: store sieve only for odd numbers

--- informatics_973/informatics_973_2.py
+++ informatics_973/informatics_973_2.py
@@ -7,6 +7,8 @@
 # 1.4 сек
 # 4. Заполнение только по нечётным производным элементам (3p, 5p и т.д.):
 # 0.8 сек
+# 5. Хранение значений решета только для нечётных чисел:
+# 1.3 сек (антиоптимизация по времени, зато оптимизация по памяти в 2 раза)
 
 from time import time
 LIMIT = 15_500_000
@@ -15,10 +17,10 @@ sieve = [True] * SIEVE_SIZE
 
 def MakeSieve():
     for p in range(3, int(SIEVE_SIZE ** 0.5 + 1), 2):
-        if not sieve[p]:
+        if not sieve[p // 2]:
             continue
         for m in range(3 * p, SIEVE_SIZE, 2 * p):
-            sieve[m] = False
+            sieve[m // 2] = False
 
 # Проверка: простые числа меньше 100
 primes_under_50 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
@@ -26,7 +28,7 @@ primes_under_50 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 def GetPrimes(upper_boundary=50):
     yield 2
     for p in range(3, upper_boundary, 2):
-        if sieve[p]:
+        if sieve[p // 2]:
             yield p
 
 t1 = time()
