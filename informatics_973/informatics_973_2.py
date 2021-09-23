9f0b6dd612933d68048a444902fdaa5580c83a1e
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-09-23 21:19:39 +0300

    973: fill the sieve using slice

--- informatics_973/informatics_973_2.py
+++ informatics_973/informatics_973_2.py
@@ -9,18 +9,25 @@
 # 0.8 сек
 # 5. Хранение значений решета только для нечётных чисел:
 # 1.3 сек (антиоптимизация по времени, зато оптимизация по памяти в 2 раза)
+# 5-я оптимизация требует 4-ю для корректной работы
+# 6. Заполнение решета через срезы
+# 0.5 сек на заполнение
 
+import math
 from time import time
+
 LIMIT = 15_500_000
 SIEVE_SIZE = LIMIT + 1
 sieve = [True] * SIEVE_SIZE
 
 def MakeSieve():
-    for p in range(3, int(SIEVE_SIZE ** 0.5 + 1), 2):
+    for p in range(3, int(math.sqrt(SIEVE_SIZE) + 1), 2):
         if not sieve[p // 2]:
             continue
-        for m in range(3 * p, SIEVE_SIZE, 2 * p):
-            sieve[m // 2] = False
+        start = (3 * p) // 2
+        step = p
+        amount = math.ceil((SIEVE_SIZE - start) / step)
+        sieve[start :: step] = [False] * amount
 
 # Проверка: простые числа меньше 100
 primes_under_50 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
