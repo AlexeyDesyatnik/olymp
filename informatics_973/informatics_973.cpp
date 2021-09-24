678c500a908d69b475285462b11fd806beb442c5
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-09-24 19:49:16 +0300

    973: optimized C++ solution so it could run circles around everything else

--- informatics_973/informatics_973.cpp
+++ informatics_973/informatics_973.cpp
@@ -1,22 +1,39 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+const int LIMIT = 15500000;
+const int SIEVE_SIZE = LIMIT / 2 + 1;
+vector<bool> sieve(SIEVE_SIZE, 1);
+
+void makeSieve() {
+    int stop = (int) sqrt(LIMIT) + 1;
+    for (int p = 3; p < stop; p += 2) {
+        if (!sieve[p / 2]) continue;
+        for (int n = p * p; n < LIMIT; n += p * 2) {
+            sieve[n / 2] = false;
+        }
+    }
+}
+
+int getNthPrime(int n) {
+    if (n == 1) return 2;
+    int primeCount = 1;
+    for (int p = 3; p < LIMIT; p += 2) {
+        if (sieve[p / 2]) {
+            primeCount++;
+            if (primeCount == n) return p;
+        }
+    }
+    return -1; // shouldn't happen
+}
+
 int main()
 {
-    const int SIEVE_SIZE = 15800000;
-    vector<bool> sieve(SIEVE_SIZE, 1);
+    makeSieve();
     int K;
     cin >> K;
-    int count = 0;
-    int i;
-    for (i = 2; count < K; i++)
-    {
-        if (!sieve[i])
-            continue;
-        count++;
-        //cout << i << endl;
-        for (int j = i + i; j < SIEVE_SIZE; j += i)
-            sieve[j] = false;
-    }
-    cout << (i - 1) << endl;
+    int p = getNthPrime(K);
+    cout << p << endl;
 }
